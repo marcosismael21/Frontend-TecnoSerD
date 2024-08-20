9def0509f927bf2e18ab48917a015db2c3a5f6b0
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2024-08-20 13:08:50 -0600

    cambios varios

--- pages/login.vue
+++ pages/login.vue
@@ -8,22 +8,25 @@
         <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
             <v-form ref="form">
                 <v-text-field v-model="logg.nombre" label="Nombre de Usuario" density="compact"
-                    placeholder="Nombre de Usuario" prepend-inner-icon="mdi-account" variant="outlined" required>
+                    placeholder="Nombre de Usuario" prepend-inner-icon="mdi-account" variant="outlined"
+                    :error-messages="nombreErrors" required>
                 </v-text-field>
 
                 <v-text-field v-model="logg.pass" label="Contraseña"
                     :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
                     density="compact" placeholder="Contraseña" prepend-inner-icon="mdi-lock-outline" variant="outlined"
-                    @click:append-inner="visible = !visible" required>
+                    @click:append-inner="visible = !visible" :error-messages="passErrors" required>
                 </v-text-field>
             </v-form>
+
+            <!-- Mensaje de error -->
+            <v-alert v-if="errorMessage" type="error" class="mb-4">
+                {{ errorMessage }}
+            </v-alert>
 
             <v-btn class="mb-8" color="blue" size="large" variant="tonal" block @click="login">
                 Log In
             </v-btn>
-
-            <v-btn color="blue darken-1" text @click="login">Login</v-btn>
-
         </v-card>
     </div>
 
@@ -34,6 +37,9 @@
 export default {
     data: () => ({
         visible: false,
+        errorMessage: '', // Almacena el mensaje de error general
+        nombreErrors: [], // Almacena los errores específicos del campo 'nombre'
+        passErrors: [], // Almacena los errores específicos del campo 'pass'
         logg: {
             nombre: '',
             pass: ''
@@ -41,14 +47,42 @@ export default {
     }),
     methods: {
         login() {
-            if (this.$refs.form.validate()) {
-                this.$axios.post('http://localhost:3010/api/v1/auth/login', this.logg)
-                    .then(() => {
-                        //this.$router.push({ name: 'index' });
-                        console.log("hola mundo")
+            this.validateFields();
+            if (this.nombreErrors.length === 0 && this.passErrors.length === 0) {
+                this.$axios.post('/auth/login', this.logg)
+                    .then((response) => {
+                        const {
+                            ok,
+                            userData,
+                            mensage
+                        } = response.data;
+
+                        if (ok) {
+                            console.log('Login exitoso:', userData);
+                            this.errorMessage = 'Login exitoso'; // Limpia el mensaje de error si es exitoso
+                        } else {
+                            console.log(ok, " b")
+                            this.errorMessage = mensage;
+                        }
                     }).catch((error) => {
-                        console.error('Error creando al cliente:', error);
+                        this.errorMessage = 'Error al iniciar sesión:', error
+                        //this.errorMessage = 'Usuario o contraseña incorrecta';
                     });
+            }
+        },
+        validateFields() {
+            // Limpia los mensajes de error anteriores
+            this.nombreErrors = [];
+            this.passErrors = [];
+
+            // Verifica si 'nombre' está vacío
+            if (!this.logg.nombre) {
+                this.nombreErrors.push('El nombre de usuario es obligatorio');
+            }
+
+            // Verifica si 'pass' está vacío
+            if (!this.logg.pass) {
+                this.passErrors.push('La contraseña es obligatoria');
             }
         }
     }
--- utils/getApiUrl.js
+++
@@ -1,4 +0,0 @@
-export const getApiUrl = async () => {
-    const apiUrl = 'http://localhost:3010/api/v1';
-    return apiUrl;
-}
