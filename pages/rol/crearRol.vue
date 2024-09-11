d4c8a26edfd65beb937b8e26233e9594bb68848d
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2024-09-11 00:51:35 -0600

    se cambio los colores a los botones y se añadio el modal de eliminar en rol

--- pages/rol/crearRol.vue
+++ pages/rol/crearRol.vue
@@ -19,8 +19,8 @@
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="yellow" text @click="closeDialog">Cancelar</v-btn>
-            <v-btn color="green" text @click="saveRol">Guardar</v-btn>
+            <v-btn color="red darken-1" text @click="closeDialog">Cancelar</v-btn>
+            <v-btn color="green darken-1" text @click="saveRol">Guardar</v-btn>
         </v-card-actions>
     </v-card>
 </template>
