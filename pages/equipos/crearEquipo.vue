48ffa64e7869624b8a514bcce878020e41377a3f
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2024-09-11 00:50:24 -0600

    se cambio los colores a los botones y se añadio el modal de eliminar en equipos

--- pages/equipos/crearEquipo.vue
+++ pages/equipos/crearEquipo.vue
@@ -48,8 +48,8 @@
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="yellow" text @click="closeDialog">Cancelar</v-btn>
-            <v-btn color="green" text @click="guardarRegistro">Guardar</v-btn>
+            <v-btn color="red darken-1" text @click="closeDialog">Cancelar</v-btn>
+            <v-btn color="green darken-1" text @click="guardarRegistro">Guardar</v-btn>
         </v-card-actions>
     </v-card>
 </template>
--- pages/equipos/detalleEquipo.vue
+++ pages/equipos/detalleEquipo.vue
@@ -37,7 +37,7 @@
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeDialog">Volver</v-btn>
+            <v-btn color="red darken-1" text @click="closeDialog">Volver</v-btn>
         </v-card-actions>
     </v-card>
     <v-alert v-else type="error">No se pudo cargar la información del equipo.</v-alert>
