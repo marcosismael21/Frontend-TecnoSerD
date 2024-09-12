69eabff0dd05ab98f7eede691ff577bc4c563829
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2024-09-12 12:58:43 -0600

    se creo el modulo de tablero, sigue en periodo de pruebas

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/axios.js'],
+  plugins: [
+    '~/plugins/axios.js',
+    { src: '~/plugins/apexcharts.js', mode: 'client' }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/tablero/index.vue
+++ pages/tablero/index.vue
@@ -1,66 +1,28 @@
 <template>
-    <div>
-      <v-progress-linear
-        v-model="value"
-        :buffer-value="bufferValue"
-      ></v-progress-linear>
-      <br>
-      <v-progress-linear
-        v-model="value"
-        :buffer-value="bufferValue"
-        color="purple"
-      ></v-progress-linear>
-      <br>
-      <v-progress-linear
-        v-model="value"
-        :buffer-value="bufferValue"
-        color="red-lighten-2"
-      ></v-progress-linear>
-      <br>
-      <v-progress-linear
-        v-model="value"
-        :buffer-value="bufferValue"
-        color="black"
-      ></v-progress-linear>
-    </div>
-  </template>
-  <script>
-    export default {
-      data () {
-        return {
-          value: 10,
-          bufferValue: 20,
-          interval: 0,
+  <div>
+    <apexchart type="bar" :options="basicOptions" :series="basicSeries" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      basicOptions: {
+        chart: {
+          type: 'bar'
+        },
+        xaxis: {
+          categories: ['A', 'B', 'C', 'D', 'E']
         }
       },
-  
-      watch: {
-        value (val) {
-          if (val < 100) return
-  
-          this.value = 0
-          this.bufferValue = 10
-          this.startBuffer()
-        },
-      },
-  
-      mounted () {
-        this.startBuffer()
-      },
-  
-      beforeUnmount () {
-        clearInterval(this.interval)
-      },
-  
-      methods: {
-        startBuffer () {
-          clearInterval(this.interval)
-  
-          this.interval = setInterval(() => {
-            this.value += Math.random() * (15 - 5) + 5
-            this.bufferValue += Math.random() * (15 - 5) + 6
-          }, 2000)
-        },
-      },
+      basicSeries: [
+        {
+          name: 'Series 1',
+          data: [10, 20, 30, 40, 50]
+        }
+      ]
     }
-  </script>
+  }
+}
+</script>
